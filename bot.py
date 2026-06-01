8a31f1f25cef1f14e004b66ca7e41dd83902e27c
chuchangmoshi-cmd <chuchangmoshi@gmail.com>
2026-06-01 18:15:09 +0800

    Update bot.py3

--- bot.py
+++ bot.py
@@ -262,67 +262,66 @@ async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
             "Please send your PUBG UID"
         )
 
-    # UID Input
+        # UID Input
     elif text.isdigit():
 
-    game = context.user_data.get("game")
-    package = context.user_data.get("package")
-    price = context.user_data.get("price")
+        game = context.user_data.get("game")
+        package = context.user_data.get("package")
+        price = context.user_data.get("price")
 
-    if game and package:
+        if game and package:
 
-        price_num = int(
-            price.replace(" KS", "")
-        )
+            price_num = int(
+                price.replace(" KS", "")
+            )
 
-        balance = get_balance(
-            update.effective_user.id
-        )
+            balance = get_balance(
+                update.effective_user.id
+            )
 
-        if balance < price_num:
+            if balance < price_num:
+
+                await update.message.reply_text(
+                    f"❌ Insufficient Balance\n\n"
+                    f"Current Balance:\n"
+                    f"{balance} KS 🇲🇲\n\n"
+                    f"Required:\n"
+                    f"{price_num} KS 🇲🇲"
+                )
+
+                return
+
+            deduct_balance(
+                update.effective_user.id,
+                price_num
+            )
+
+            new_balance = get_balance(
+                update.effective_user.id
+            )
 
             await update.message.reply_text(
-                f"❌ Insufficient Balance\n\n"
-                f"Current Balance:\n"
-                f"{balance} KS 🇲🇲\n\n"
-                f"Required:\n"
-                f"{price_num} KS 🇲🇲"
-            )
-
-            return
-
-        deduct_balance(
-            update.effective_user.id,
-            price_num
-        )
-
-        new_balance = get_balance(
-            update.effective_user.id
-        )
-
-        await update.message.reply_text(
-            f"✅ Order Submitted\n\n"
-            f"Game: {game}\n"
-            f"Package: {package}\n"
-            f"Price: {price} 🇲🇲\n"
-            f"Player ID: {text}\n\n"
-            f"Remaining Balance:\n"
-            f"{new_balance} KS 🇲🇲"
-        )
-
-        await context.bot.send_message(
-            chat_id=ADMIN_ID,
-            text=(
-                f"📦 New Order\n\n"
-                f"User: @{update.effective_user.username}\n"
-                f"User ID: {update.effective_user.id}\n\n"
+                f"✅ Order Submitted\n\n"
                 f"Game: {game}\n"
                 f"Package: {package}\n"
-                f"Price: {price}\n"
-                f"Player ID: {text}"
+                f"Price: {price} 🇲🇲\n"
+                f"Player ID: {text}\n\n"
+                f"Remaining Balance:\n"
+                f"{new_balance} KS 🇲🇲"
             )
-        )
 
+            await context.bot.send_message(
+                chat_id=ADMIN_ID,
+                text=(
+                    f"📦 New Order\n\n"
+                    f"User: @{update.effective_user.username}\n"
+                    f"User ID: {update.effective_user.id}\n\n"
+                    f"Game: {game}\n"
+                    f"Package: {package}\n"
+                    f"Price: {price}\n"
+                    f"Player ID: {text}"
+                )
+            )
         await start(update, context)
 
 
