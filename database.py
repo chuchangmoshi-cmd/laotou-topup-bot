46a7745bd209aa82041ce2ee8fe6556ae70f7138
chuchangmoshi-cmd <chuchangmoshi@gmail.com>
2026-06-02 03:09:06 +0800

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,29 @@
+import sqlite3
 
+DB_FILE = "database.db"
+
+def init_db():
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS balances (
+            user_id TEXT PRIMARY KEY,
+            balance INTEGER DEFAULT 0
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS orders (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id TEXT,
+            game TEXT,
+            package TEXT,
+            price TEXT,
+            uid TEXT,
+            status TEXT
+        )
+    """)
+
+    conn.commit()
+    conn.close()
